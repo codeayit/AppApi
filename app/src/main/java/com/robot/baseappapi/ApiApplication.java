b1e323f59f5bd3b48c54c7e5f26993b80eb52690
1053452188@qq.com <lny199211>
2018-11-28 14:42:45 +0800

    全局 filter

--- app/src/main/java/com/robot/baseappapi/ApiApplication.java
+++ app/src/main/java/com/robot/baseappapi/ApiApplication.java
@@ -8,6 +8,8 @@ import com.robot.baseapi.base.BaseApplication;
 import com.robot.baseapi.net.GlobalFilter;
 import com.robot.baseapi.net.NetWork;
 
+import java.util.HashMap;
+
 /**
  * Created by lny on 2018/1/15.
  */
@@ -17,12 +19,18 @@ public class ApiApplication extends BaseApplication {
     public void onCreate() {
         super.onCreate();
 
-        NetWork.setGlobalCodeFilter(new GlobalFilter() {
+        NetWork.setGlobalFilter(new GlobalFilter() {
             @Override
-            public void onCode(int code,String json) {
+            public void onPreResponse(int code, String json) {
                 KLog.d("全局code 过滤： "+code);
                 Toast.makeText(getApplicationContext(),"code:"+code,Toast.LENGTH_LONG).show();
             }
+
+            @Override
+            public void onPreRequest(String url, HashMap<String, String> headers, HashMap<String, String> params) {
+
+            }
+
         });
     }
 }
