0c85f39faaf8991eada04a3679554d86388ff6cf
1053452188@qq.com <lny199211>
2018-12-04 14:29:10 +0800

    去掉VoicePlayer

--- baseapi/src/main/java/com/robot/baseapi/util/FileUtil.java
+++ baseapi/src/main/java/com/robot/baseapi/util/FileUtil.java
@@ -107,4 +107,66 @@ public class FileUtil {
 
         }
     }
+
+    public static String readFile(String path){
+        return readFile(path,1024);
+    }
+
+    public static String readFile(String path,int bufferSize){
+        StringBuilder sb =  new StringBuilder();
+        InputStream fis = null;
+        try {
+            fis = new FileInputStream(path);
+            byte[] buffer = new byte[bufferSize];
+
+            int len = 0;
+            while ((len = fis.read(buffer))!=-1){
+                sb.append(new String(buffer,0,len));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+//            MyLog.JUN_KANG.d(" ***ERROR*** read file: " + e.getMessage());
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    fis = null;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String readAssetsFile(Context context,String path){
+        return readAssetsFile(context,path,1024);
+    }
+
+    public static String readAssetsFile(Context context,String path,int bufferSize){
+        StringBuilder sb =  new StringBuilder();
+        InputStream fis = null;
+        try {
+            fis = context.getAssets().open(path);
+            byte[] buffer = new byte[bufferSize];
+            int len = 0;
+            while ((len = fis.read(buffer))!=-1){
+                sb.append(new String(buffer,0,len));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    fis = null;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
 }
