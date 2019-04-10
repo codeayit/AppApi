0d6880ee94c3bbf12b9f901eae66e6046c4fef18
北漂lny <1053452188@qq.com>
2019-04-10 09:44:04 +0800

    AsyncTaskUtil  完善

--- baseapi/src/main/java/com/robot/baseapi/util/AsyncTaskUtil.java
+++ baseapi/src/main/java/com/robot/baseapi/util/AsyncTaskUtil.java
@@ -38,6 +38,10 @@ public class AsyncTaskUtil {
         }
     }
 
+    /**
+     * 么有返回结果
+     * @param task
+     */
     public static void run(Task2 task){
         final Task2 t = task;
         if (task!=null){
@@ -62,7 +66,64 @@ public class AsyncTaskUtil {
     }
 
 
+    /**
+     *
+     * @param task
+     * @param <T> 返回结果
+     */
+    public static<T> void run(Task3<T> task){
+        final Task3<T> t = task;
+        if (task!=null){
+            t.before(t.getParam());
+            Observable.just(t.getParam()).map(
+                    new Function<T, T>() {
+                        @Override
+                        public T apply(T value) throws Exception {
+                             t.taskRun(value);
+                             return value;
+                        }
+                    })
 
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<T>() {
+                        @Override
+                        public void accept(T value) throws Exception {
+                            t.success(value);
+                        }
+                    });
+        }
+    }
+
+
+    /**
+     *
+     * @param task
+     * @param <P> 参数
+     * @param <T> 返回结果
+     */
+    public static<P,T> void run(Task4<P,T> task){
+        final Task4<P,T> t = task;
+        if (task!=null){
+            t.before(t.getParam());
+            Observable.just(t.getParam()).map(
+                    new Function<P, T>() {
+                        @Override
+                        public T apply(P value) throws Exception {
+                            return t.taskRun(value);
+                        }
+                    })
+
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<T>() {
+                        @Override
+                        public void accept(T value) throws Exception {
+                            t.success(t.getParam(),value);
+                        }
+                    });
+        }
+    }
 
 
 
@@ -73,9 +134,44 @@ public class AsyncTaskUtil {
     }
 
     public static abstract class Task2 {
+        public Task2() {
+        }
+
         public void before(){};
         public abstract void taskRun();
         public void success(){};
+    }
+
+    public static abstract class Task3<P> {
+        private P param;
+
+        public Task3(P param) {
+            this.param = param;
+        }
+
+        public P getParam() {
+            return param;
+        }
+
+        public abstract void before(P param);
+        public abstract void taskRun( P param);
+        public abstract void success(P param);
+    }
+
+    public static abstract class Task4<P,T> {
+        private P param;
+
+        public Task4(P param) {
+            this.param = param;
+        }
+
+        public P getParam() {
+            return param;
+        }
+
+        public abstract void before(P param);
+        public abstract T taskRun( P param);
+        public abstract void success(P param,T t);
     }
 
 }
